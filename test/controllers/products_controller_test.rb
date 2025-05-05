81cb0858b8336542aea11a26c7d5d3c318689158
pablohaller <pablo.haller.dev@gmail.com>
2025-05-05 17:35:42 -0300

    feat: add more cart related items

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -39,11 +39,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
-  test "can't delete product in cart" do 
+  test "can't delete product in cart" do
     assert_difference("Product.count", 0) do
-      delete product_url(@product)
+      delete product_url(products(:two))
     end
-
     assert_redirected_to products_url
   end
 
