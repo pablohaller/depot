632a2eb0a5dd5994a5b592f8b066cdc9e3914270
pablohaller <pablo.haller.dev@gmail.com>
2025-09-21 20:41:57 -0300

    iteration i4, page 225

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  after_destroy :ensure_an_admin_remains
+  class Error < StandardError
+  end
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise Error.new "Can't delete last user"
+      end
+    end
 end
